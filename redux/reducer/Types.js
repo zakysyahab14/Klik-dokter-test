3f7879ef1d72a1f31dc2e254043dc13df20bea80
Zaky Syahab <zack4tress@gmail.com>
2022-01-17 23:48:23 +0700

    first commit

--- redux/reducer/Types.js
+++ redux/reducer/Types.js
@@ -2,10 +2,6 @@ export const USER_LOGIN = "USER_LOGIN";
 export const USER_LOGOUT = "USER_LOGOUT";
 export const USER_REGISTER = "USER_REGISTER";
 
-export const GET_USERS = "GET_USERS"
-export const ADD_USERS = "ADD_USERS";
-export const DELETE_USERS = "DELETE_USERS";
-
 export const GET_PRODUCT = "GET_PRODUCT"
 export const ADD_PRODUCT = "ADD_PRODUCT";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
--- redux/reducer/index.js
+++ redux/reducer/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import LoginReducers from './LoginReducers';
-import UsersReducers from './UsersReducers';
 import ProductReducers from './ProductReducers';
 
 export default combineReducers({
     Login: LoginReducers,
-    Users: UsersReducers,
     Product: ProductReducers,
 })
